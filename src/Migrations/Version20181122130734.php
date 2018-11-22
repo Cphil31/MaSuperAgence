0d3ad7189a485dd8ee91bd5b31e0fdc16f652bef
unknown <cecilephilippe31@gmail.com>
2018-11-22 14:12:44 +0100

    ajout de champs dans la table property

--- src/Migrations/Version20181122130734.php
+++ src/Migrations/Version20181122130734.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181122125751 extends AbstractMigration
+final class Version20181122130734 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
